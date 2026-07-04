70d4faf5154b44a672d83f3213986019f4e45134
tonyzxu <tony.z.xu@gmail.com>
2026-07-03 22:11:13 -0700

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        product: resolve(__dirname, 'product.html'),
+      },
+    },
+  },
+})
